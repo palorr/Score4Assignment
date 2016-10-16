7aba0cefccb9396081675c5329e9035f2606b575
palorr <archontellis29@gmail.com>
2016-10-16 23:57:32 +0300

    initial
    elpizw na ta katafera

---
+++ Score-4/State.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Score_4
+{
+    class State
+    {
+        private int[,] Cells { get; set; }
+        private string[,] CellsNames;
+        //
+        public State()//ctor1
+        {
+            Cells = null;
+            //
+            for (int i = 0; i < 7; i++)//row
+            {
+                for (int j = 0; j < 7; j++)//col
+                {
+                    CellsNames[i, j] = "c" + i + j; 
+                }
+            }
+            //
+        }
+        //
+        public void push(int col)//col=0-6 για να μην εχουμε 7 διαφορετικες που δεν σου αρεσε 
+        {
+
+        }
+        
+
+    }
+}
